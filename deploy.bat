89b0ea1a5b9e15ff736e61d7aced6ef1db23af5d
mark-summerfield <mark@qtrac.eu>
2022-01-13 13:23:24 +0000

    tiny fix

--- deploy.bat
+++ deploy.bat
@@ -2,5 +2,5 @@ SET CARGO_TARGET_DIR=target
 SET RELEASE_DIR=%CARGO_TARGET_DIR%\release
 cargo build --release -- %*
 copy /Y target\release\amp-rs.exe amp.exe
-rcedit amp.exe --set-icon images\amp.ico
+rcedit amp.exe --set-icon data\amp.ico
 copy /Y amp.exe C:\bin
